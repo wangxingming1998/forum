60f8f604c6de030629c4e179328f25c0521fe32e
sa <2240846874@qq.com>
2019-08-26 16:12:47 +0800

    second commit

---
+++ src/main/java/com/t98k/forum/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.t98k.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author sa
+ * @date 2019/8/26
+ * @description
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+
+}
